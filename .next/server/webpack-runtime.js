df033308bffc3747890b24612a1c52b131647cbe
mohammad <ghaffari8343@gmail.com>
2024-12-19 14:46:54 +0330

    update website's name

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -130,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("ed8a8bb96c9090f1")
+/******/ 		__webpack_require__.h = () => ("c892959371733f9f")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f2ca524e45f47367"; }
+/******/ 		__webpack_require__.h = function() { return "3842022a347933b6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
